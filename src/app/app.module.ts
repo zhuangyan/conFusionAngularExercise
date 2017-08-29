a68c4843cecfd997033ca31a89a291e256c4e9cc
zhuangyan <zy_php@163.com>
2017-08-29 17:29:42 +0800

    Basic Service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import 'hammerjs';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +24,7 @@ import { DishdetailComponent } from './dishdetail/dishdetail.component';
     MaterialModule,
     FlexLayoutModule
   ],
-  providers: [],
+  providers: [DishService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { DISHES } from '../shared/dishes';
+import { DishService } from '../services/dish.service';
+
 
 
 
@@ -12,9 +14,10 @@ import { DISHES } from '../shared/dishes';
 export class MenuComponent implements OnInit {
   dishes = DISHES;
   selectedDish:Dish;
-  constructor() { }
-
+  constructor(private dishService: DishService) { }
+  
   ngOnInit() {
+    this.dishes = this.dishService.getDishes();    
   }
   onSelect(dish: Dish) {
     this.selectedDish = dish;
