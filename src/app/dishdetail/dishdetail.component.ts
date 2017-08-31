109a971a45360d7f691b4b44bc3ec78f64090b27
zhuangyan <zy_php@163.com>
2017-08-31 18:13:24 +0800

    第三周作业

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, Input } from '@angular/core';
-import 'rxjs/add/operator/switchMap';
 
 import { Dish } from '../shared/dish';
+
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+import { Comment } from '../shared/comment';
+
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-dishdetail',
@@ -16,31 +20,97 @@ import { Location } from '@angular/common';
 })
 export class DishdetailComponent implements OnInit {
 
-   @Input() 
-   dish:Dish;
-   dishIds: number[];
-   prev: number;
-   next: number;
+  dish: Dish;
 
-   constructor(private dishservice: DishService,
+  dishIds: number[];
+  prev: number;
+  next: number;
+
+  commentForm: FormGroup;
+  newComment: Comment;
+
+  constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location, private cm: FormBuilder) 
+    {
+      this.createForm();
+     }
 
   ngOnInit() {
-    this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
-    this.route.params
+      this.dishservice.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+      this.route.params
       .switchMap((params: Params) => this.dishservice.getDish(+params['id']))
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
-    }
-    
-  setPrevNext(dishId: number) {
-      let index = this.dishIds.indexOf(dishId);
-      this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
-      this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
-    }  
+  }
+
+   setPrevNext(dishId: number) {
+    let index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1)%this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1)%this.dishIds.length];
+  }
 
   goBack(): void {
     this.location.back();
   }
 
+  formErrors = {
+    'author': '',
+    'comment': ''
+  };
+
+  validationMessages = {
+    'author': {
+      'required':      'Name is required.',
+      'minlength':     'Name must be at least 2 characters long.',
+      'maxlength':     'Name cannot be more than 25 characters long.'
+    },
+    'comment': {
+      'required':      'Comment is required.',
+      'minlength':     'Comment must be a word.',
+      'maxlength':     'Comment cannot be more than 250 characters long.'
+    }
+  };
+
+  createForm(): void {
+    this.commentForm = this.cm.group({
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      comment: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(250)] ],
+      rating: [5,[]]
+    });
+    this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set validation messages now
+
+  }
+
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+  onSubmit() {
+    this.newComment = this.commentForm.value;
+// Upon submission of the comment, it gets included in the regular comments and shown on the screen
+    var d = new Date();
+    this.newComment.date = d.toISOString();
+    this.dish.comments.push(this.newComment);
+//end of insertion logic
+    console.log(this.newComment);
+    this.commentForm.reset({
+      author: '',
+      comment: '',
+      rating : "5"
+    });
+  }
 }
